28cd130990755547c58f9e8fe0242b77f361f287
David Feeley <davidpfeeley@gmail.com>
2022-09-13 23:14:08 -0700

    fixed merge conflict

--- pages/knowledge/index.vue
+++ pages/knowledge/index.vue
@@ -7,11 +7,7 @@
         <NuxtLink
           v-for="page in knowledgeCenterPages"
           :key="page._id"
-<<<<<<< HEAD
-          :to="`/knowledge/${page.slug}`"
-=======
           :to="`/knowledge/${page.slug.current}`"
->>>>>>> Added knowledge index page to make sure we generate the various dynamic pages, and reverted nuxt.config generate: true setting
         >{{ page.name }}
         </NuxtLink>
       </li>
